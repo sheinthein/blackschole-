23864e7a527a1aa31822c70b0999794b2eb419c5
sheinthein <harry930308@gmail.com>
2024-08-19 23:23:22 -0400

    Update stock_option_app.py

--- stock_option_app.py
+++ stock_option_app.py
@@ -1,4 +1,17 @@
 
+from pathlib import Path
+
+import appdirs as ad
+
+CACHE_DIR = ".cache"
+
+# Force appdirs to say that the cache dir is .cache
+ad.user_cache_dir = lambda *args: CACHE_DIR
+
+# Create the cache dir if it doesn't exist
+Path(CACHE_DIR).mkdir(exist_ok=True)
+
+import yfinance as yf
 import streamlit as st
 import yfinance as yf
 import numpy as np
